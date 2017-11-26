a7db6ac780f8f9926f34600f707c52e60ddbc294
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-11-26 08:41:37 -0200

    small changes

--- application/routes/format.js
+++ application/routes/format.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var formatter = require('../services/formatter.js');
 
-/* GET users listing. */
 router.post('/', function(req, res, next) {
   let format = new formatter(req,res);
   format.beautify();
