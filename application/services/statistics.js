b2a6de7ce1b06674812129e7d1976b4c53115a60
Lucas Meine <lucas.meine@citara.io>
2018-01-02 15:16:31 -0200

    add tracking badge

--- application/services/statistics.js
+++ application/services/statistics.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 const file_name = "./application/includes/stats.txt";
 var utilities = require("../helpers/utils.js");
 var request = require("request");
+var path = require("path");
 var utils = new utilities();
 module.exports = Statistics;
 
@@ -26,9 +27,16 @@ function read_file(file_name, operation) {
 
 global.print = function(file_name, data) {
   request(
-    "https://img.shields.io/badge/beautified-"+data.trim()+"%20files-brightgreen.svg?logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAAA0AAAASCAQAAAAqYpy5AAAA70lEQVQY023RPS5EARTF8TMfjc8oNEKtE5nCHiQWoFZIxAZ0NqASiUqjtARREVqJWkRBIxHMSIiZee%2BnGA8j7i1P7j%2FnnhPX9jRFQwb7Pd5xYl401IZEbSUerIqo%2F7rVQQ%2FsGhsC64BCgQsLP%2BBKgi5erFXgSmrr4gMcmBL16ChxYtn9N%2FjSksSzvsKZmHGoVOihbbmZ8TSSTCR5ykOSWmqRyczFqTu3trVcodTDqw3NGDVh2pZXdJU41xqYj3nHfPkr7RgdPB7rHtHTx42Vn7jy658js0Mhe1GgbVP%2BVOMNpxZFvcq9KuXavpH%2FqvwEzYI0hSs7EJ8AAAAASUVORK5CYII%3D",
+    "https://img.shields.io/badge/beautified-" +
+      data.trim() +
+      "%20files-brightgreen.svg?logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAAA0AAAASCAQAAAAqYpy5AAAA70lEQVQY023RPS5EARTF8TMfjc8oNEKtE5nCHiQWoFZIxAZ0NqASiUqjtARREVqJWkRBIxHMSIiZee%2BnGA8j7i1P7j%2FnnhPX9jRFQwb7Pd5xYl401IZEbSUerIqo%2F7rVQQ%2FsGhsC64BCgQsLP%2BBKgi5erFXgSmrr4gMcmBL16ChxYtn9N%2FjSksSzvsKZmHGoVOihbbmZ8TSSTCR5ykOSWmqRyczFqTu3trVcodTDqw3NGDVh2pZXdJU41xqYj3nHfPkr7RgdPB7rHtHTx42Vn7jy658js0Mhe1GgbVP%2BVOMNpxZFvcq9KuXavpH%2FqvwEzYI0hSs7EJ8AAAAASUVORK5CYII%3D",
     function(error, response, body) {
-      global.response.send(body);
+      fs.writeFile('./application/includes/badge.svg', body, function(err) {
+        
+        global.response.sendFile(path.resolve('./application/includes/badge.svg'));
+
+      });
+
     }
   );
 };
